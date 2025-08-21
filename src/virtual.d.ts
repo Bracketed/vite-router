a96bfe4d4ad5e0f9f4d864c6dcd5cdbf9c4b2646
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-08-21 23:51:33 +0100

    feat: update version to 2.0.6 and define VitePagesPluginProps interface in virtual.d.ts

--- src/virtual.d.ts
+++ src/virtual.d.ts
@@ -1,7 +1,14 @@
 /// <reference types="react" />
 
-import type { VitePagesPluginProps } from './types/Props';
-
 declare module 'virtual:vite-plugin-router' {
+	interface VitePagesPluginProps {
+		/**
+		 * If we should use a different loading component instead of a <div>Loading...</div>
+		 *
+		 * @default undefined
+		 */
+		loadingPage?: React.ReactNode;
+	}
+
 	export const AppRoutes: React.FC<VitePagesPluginProps>;
 }
