dd4372abc693709ec6d15d89a1464ead91d4ba16
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-08-21 23:39:53 +0100

    feat: update version to 2.0.4 and specify AppRoutes type in virtual.d.ts

--- src/virtual.d.ts
+++ src/virtual.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="react" />
 
+import type { VitePagesPluginProps } from './types/Props.types';
+
 declare module 'virtual:vite-plugin-router' {
-	export const AppRoutes: React.FC;
+	export const AppRoutes: React.FC<VitePagesPluginProps>;
 }
